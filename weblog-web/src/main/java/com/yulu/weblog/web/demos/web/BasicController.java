4bed5684fc816a4c47ee86de74eb1f587f35afca
3321 <1378425995@qq.com>
2025-05-07 18:03:11 +0800

    feat：自定义响应工具类

--- weblog-web/src/main/java/com/yulu/weblog/web/demos/web/BasicController.java
+++ weblog-web/src/main/java/com/yulu/weblog/web/demos/web/BasicController.java
@@ -17,15 +17,14 @@
 package com.yulu.weblog.web.demos.web;
 
 import com.yulu.weblog.common.aspect.ApiOperationLog;
+import com.yulu.weblog.common.utils.Response;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import java.util.stream.Collectors;
 
@@ -38,7 +37,7 @@ public class BasicController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+    public Response test(@RequestBody @Validated User user, BindingResult bindingResult) {
         // 是否存在校验错误
         if (bindingResult.hasErrors()) {
             // 获取校验不通过字段的提示信息
@@ -47,11 +46,11 @@ public class BasicController {
                     .map(FieldError::getDefaultMessage)
                     .collect(Collectors.joining(", "));
 
-            return ResponseEntity.badRequest().body(errorMsg);
+            return Response.fail(errorMsg);
         }
 
         // 返参
-        return ResponseEntity.ok("参数没有任何问题");
+        return Response.success();
     }
 
 }
